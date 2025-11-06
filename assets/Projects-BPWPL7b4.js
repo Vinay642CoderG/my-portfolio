bca44f41054631cd4980ceec9a2d5caf62976719
Vinay642CoderG <149595898+Vinay642CoderG@users.noreply.github.com>
2025-11-06 19:03:31 +0530

    Updates

--- assets/Projects-BPWPL7b4.js
+++ assets/Projects-BPWPL7b4.js
@@ -1,4 +1,4 @@
-import{s as a,p as c,d as l,r as d,j as e,q as j,t as x,n as m,o as h}from"./index-D62kSyIa.js";import{P as g,a as b,b as P,c as u,d as f,e as p,f as k,g as v,h as L,i as T,T as w,j as A,k as r}from"./styles-B5p3hJzY.js";const B=a.div`
+import{s as a,p as c,d as l,r as d,j as e,q as j,t as x,n as m,o as h}from"./index-DdvVz7r-.js";import{P as g,a as b,b as P,c as u,d as f,e as p,f as k,g as v,h as L,i as T,T as w,j as A,k as r}from"./styles-ifbG6j2-.js";const B=a.div`
   display: flex;
   align-items: center;
   gap: 0.5rem;
